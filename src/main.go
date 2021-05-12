056e4458e53a9944f83d26212f11690440db010c
GemTay <gemma.taylor@digital.justice.gov.uk>
2021-05-12 15:34:40 +0100

    created server and handler to get basic hello world page

---
+++ src/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	l := log.New(os.Stdout, "basic-app", log.LstdFlags)
+
+	sm := mux.NewRouter()
+
+	sm.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+		l.Println("Running the hello world handler")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello world!"))
+	})
+
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		ErrorLog:     l,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Minute,
+		WriteTimeout: 15 * time.Minute,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+		l.Println("Server is up and running!")
+	}()
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	sig := <-c
+	l.Println("Recieve terminate, gracefully shutting down", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+}
